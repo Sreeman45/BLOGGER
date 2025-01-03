2443fd94aa2cbf076ea5fcd778f695b86760bcd4
sreeman <sreemanlingala333@gmail.com>
2025-01-03 18:08:49 +0530

    typo error

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cookiePaser = require("cookie-parser");
 const Blog = require("./models/blog");
 
 const userRoute = require("./routes/user");
-const blogRoute = require("./routes/blog");s
+const blogRoute = require("./routes/blog");
 
 const {
   checkForAuthenticationCookie,
